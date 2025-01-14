77b5f9c930e80b4337574693b35e22d978da50cf
AkashNegi9690 <akashnegi9690@gmail.com>
2025-01-14 18:39:42 +0530

    Updated port binding to use process.env.PORT

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1",rootRouter);
 
-app.listen(3000);
+const port = process.env.PORT || 3000;  
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
